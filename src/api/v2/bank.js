aa695d3545646ac5610b4e52dafd6a8e426b9089
nicefella <is.baygin@gmail.com>
2023-11-27 16:43:05 +0300

    final 1

--- src/api/v2/bank.js
+++ src/api/v2/bank.js
@@ -3,11 +3,176 @@
 // const errors = require('restify-errors');
 const mailer = require('../../mailer');
 // const nodemailerNTLMAuth = require('nodemailer-ntlm-auth');
+const qs = require('../../qs');
+
+
+const COLOR_POSITIVE = '#49a127';
+const COLOR_NEGATIVE = '#bf3032';
+
+
+const BANK_CONFIG = {
+     appId: 'e28a113f-71f3-41ad-afd6-ffeee9776fcf',
+
+     kpi_bankadaki_para: 'yJPQeRV',
+     kpi_baglanan_para: 'ESNpYgm',
+     kpi_blokeli_para: 'LkepK',
+     kpi_toplam_para: 'AUpEj',
+
+     kpi_gecmis_fkv: '2933e879-255a-487d-924b-fec74f32cc48',
+     kpi_kalan_fkv: 'c244c4a9-4021-4a08-be3d-78748984da8c',
+     kpi_toplam_fkv: 'aa1763f8-1615-4fb5-9240-6e49466da6da',
+     kpi_gecmis_fg: 'd9ac3297-8c0d-4fe7-b90d-b61504d5c70d',
+     kpi_kalan_fg: 'aad39582-4f99-4d80-9440-fd6af4f2b274',
+     kpi_toplam_fg: 'bcf74869-3107-497e-9979-fa4846e55f42',
+     kpi_gecmis_fark: '17f6c863-8226-4814-9b8d-f353502cbfe1',
+     kpi_kalan_fark: 'bdc09ec3-c328-46bf-b614-93f46cc76cc9',
+     kpi_toplam_fark: 'ca2337f1-4ef2-4e8a-a272-e233b62475ab',
+
+     tavsiye_cizelge_table: 'cc22021e-4a0b-474e-9150-dba9822fd0cc',
+     subeTable: 'BuxMPFy',
+};
+
+
+async function getKpiData({ app, objectId, useColoring = false }) {
+     const object = await app.getObject(objectId);
+     const cube = await object.getHyperCubeData('/qHyperCubeDef', [
+          {
+               qLeft: 0,
+               qTop: 0,
+               qWidth: 1,
+               qHeight: 1
+          },
+     ]);
+     const [{ qMatrix: matrix = [] } = {}] = cube || [];
+
+     const colorStyle = matrix[0][0].qNum >= 0
+          ? { color: COLOR_POSITIVE } : { color: COLOR_NEGATIVE };
+     return {
+          style: useColoring ? colorStyle : {},
+          value: matrix[0][0].qText
+     };
+}
+
 
 module.exports = {
      get: async (req, res) => {
           try {
-               mailer.sendMail('ismail@noblapps.io', 'bankadurum', {});
+               const session = await qs.init();
+               const app = await session.openDoc(BANK_CONFIG.appId, '', '', '', false);
+               const table1Object = await app.getObject(BANK_CONFIG.tavsiye_cizelge_table);
+               const cubeData1 = await table1Object.getHyperCubeData('/qHyperCubeDef', [
+                    {
+                         qLeft: 0,
+                         qTop: 0,
+                         qWidth: 18,
+                         qHeight: 100
+                    },
+               ]);
+               const [{ qMatrix: faizOranlariVeMevduatlarData = [] } = {}] = cubeData1 || [];
+
+               const subeTableObject = await app.getObject(BANK_CONFIG.subeTable);
+               const subeTableData = await subeTableObject.getHyperCubeData('/qHyperCubeDef', [
+                    {
+                         qLeft: 0,
+                         qTop: 0,
+                         qWidth: 18,
+                         qHeight: 100
+                    },
+               ]);
+               const [{ qMatrix: subeData = [] } = {}] = subeTableData || [];
+
+               const { value: kpi_baglanan_para, style: kpi_baglanan_para_style } = await getKpiData({
+                    app, objectId: BANK_CONFIG.kpi_baglanan_para, useColoring: false
+               });
+
+               const { value: kpi_bankadaki_para, style: kpi_bankadaki_para_style } = await getKpiData({
+                    app, objectId: BANK_CONFIG.kpi_bankadaki_para, useColoring: false
+               });
+
+               const { value: kpi_blokeli_para, style: kpi_blokeli_para_style } = await getKpiData({
+                    app, objectId: BANK_CONFIG.kpi_blokeli_para, useColoring: false
+               });
+
+               const { value: kpi_toplam_para, style: kpi_toplam_para_style } = await getKpiData({
+                    app, objectId: BANK_CONFIG.kpi_toplam_para, useColoring: false
+               });
+
+
+               const { value: kpi_gecmis_fkv, style: kpi_gecmis_fkv_style } = await getKpiData({
+                    app, objectId: BANK_CONFIG.kpi_gecmis_fkv, useColoring: false
+               });
+
+               const { value: kpi_kalan_fkv, style: kpi_kalan_fkv_style } = await getKpiData({
+                    app, objectId: BANK_CONFIG.kpi_kalan_fkv, useColoring: false
+               });
+
+               const { value: kpi_toplam_fkv, style: kpi_toplam_fkv_style } = await getKpiData({
+                    app, objectId: BANK_CONFIG.kpi_toplam_fkv, useColoring: false
+               });
+
+
+               const { value: kpi_gecmis_fg, style: kpi_gecmis_fg_style } = await getKpiData({
+                    app, objectId: BANK_CONFIG.kpi_gecmis_fg, useColoring: true
+               });
+
+               const { value: kpi_kalan_fg, style: kpi_kalan_fg_style } = await getKpiData({
+                    app, objectId: BANK_CONFIG.kpi_kalan_fg, useColoring: true
+               });
+
+               const { value: kpi_toplam_fg, style: kpi_toplam_fg_style } = await getKpiData({
+                    app, objectId: BANK_CONFIG.kpi_toplam_fg, useColoring: true
+               });
+
+
+               const { value: kpi_gecmis_fark, style: kpi_gecmis_fark_style } = await getKpiData({
+                    app, objectId: BANK_CONFIG.kpi_gecmis_fark, useColoring: true
+               });
+
+               const { value: kpi_kalan_fark, style: kpi_kalan_fark_style } = await getKpiData({
+                    app, objectId: BANK_CONFIG.kpi_kalan_fark, useColoring: true
+               });
+
+               const { value: kpi_toplam_fark, style: kpi_toplam_fark_style } = await getKpiData({
+                    app, objectId: BANK_CONFIG.kpi_toplam_fark, useColoring: true
+               });
+
+
+               mailer.sendMail('ismail@noblapps.io', 'bankadurum', {
+                    faizOranlariVeMevduatlarData,
+                    subeData,
+
+                    kpi_bankadaki_para,
+                    kpi_bankadaki_para_style,
+                    kpi_baglanan_para,
+                    kpi_baglanan_para_style,
+                    kpi_blokeli_para,
+                    kpi_blokeli_para_style,
+                    kpi_toplam_para,
+                    kpi_toplam_para_style,
+
+                    kpi_gecmis_fkv,
+                    kpi_gecmis_fkv_style,
+                    kpi_kalan_fkv,
+                    kpi_kalan_fkv_style,
+                    kpi_toplam_fkv,
+                    kpi_toplam_fkv_style,
+
+                    kpi_gecmis_fg,
+                    kpi_gecmis_fg_style,
+                    kpi_kalan_fg,
+                    kpi_kalan_fg_style,
+                    kpi_toplam_fg,
+                    kpi_toplam_fg_style,
+
+                    kpi_gecmis_fark,
+                    kpi_gecmis_fark_style,
+                    kpi_kalan_fark,
+                    kpi_kalan_fark_style,
+                    kpi_toplam_fark,
+                    kpi_toplam_fark_style,
+
+               });
+
                return res.send(JSON.stringify({
                     result: 'success v2',
                     // message: `${bids.length} teklif e-posta olarak gönderildi!`
