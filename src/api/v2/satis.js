c927b5f07ad4f293f20276333bd5f514bb1e14a8
nicefella <is.baygin@gmail.com>
2024-04-29 16:13:08 +0300

    satis

---
+++ src/api/v2/satis.js
@@ -0,0 +1,60 @@
+/* eslint-disable quotes */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable camelcase */
+// const errors = require('restify-errors');
+
+const mailer = require("../../mailer");
+// const nodemailerNTLMAuth = require('nodemailer-ntlm-auth');
+const qs = require("../../qs");
+
+const CONFIG = require("./../../../config.json");
+const getFormatedToday = require("../../helpers/getFormatedToday");
+const tagPivotData = require("../../helpers/tagPivotData");
+
+async function sendSatisEmail(topCubeLayout) {
+     const taggedData = tagPivotData(topCubeLayout);
+     const locals = {
+          guncelleme_tarihi: getFormatedToday(),
+          data: taggedData,
+     };
+
+     mailer.sendMail({ to: CONFIG.recipients.satis.to }, "satis", locals, {});
+}
+
+const {
+     satis: { appId, tableTopObjectId, tableBottomObjectId },
+} = CONFIG; // '072ef435-96bb-4760-bfac-3452a0f7b720';
+
+module.exports = {
+     get: async (req, res) => {
+          try {
+               console.log("notifyweekly");
+               const session = await qs.init();
+               const app = await session.openDoc(appId, "", "", "", false);
+               const topTableObject = await app.getObject(tableTopObjectId);
+               // const bottomTableObject = await app.getObject(
+               //      tableBottomObjectId
+               // );
+
+               const topCubeLayout = await topTableObject.getLayout();
+
+               await sendSatisEmail(topCubeLayout);
+
+               qs.close();
+
+               return res.send(
+                    JSON.stringify({
+                         result: "success bidweekly v2",
+                    })
+               );
+          } catch (ex) {
+               qs.close();
+               return res.send(
+                    JSON.stringify({
+                         result: "error bidweekly v2",
+                         message: ex.message,
+                    })
+               );
+          }
+     },
+};
---
+++ src/helpers/tagPivotData.js
@@ -0,0 +1,51 @@
+/* eslint-disable arrow-parens */
+/* eslint-disable quotes */
+module.exports = async function tagPivotData(layout) {
+     const { qHyperCube: { qPivotDataPages = [] } = {} } = layout;
+     const [firstPage = {}] = qPivotDataPages;
+     //  const session = await qs.init();
+     //  const app = await session.openDoc(appId, "", "", "", false);
+     //   const topTableObject = await app.getObject(tableTopObjectId);
+     //   const layout = await topTableObject.getLayout();
+
+     const dims = firstPage.qLeft;
+     const rows = firstPage.qData;
+
+     // const dims =
+     //      data.result.qLayout[0].value.qHyperCube.qPivotDataPages[0].qLeft;
+     // const rows =
+     //      data.result.qLayout[0].value.qHyperCube.qPivotDataPages[0].qData;
+
+     let refRowIndex = 0;
+     dims.forEach((dim, dimIndex) => {
+          if (dimIndex === 0) {
+               // total row
+               rows[refRowIndex] = [
+                    "total",
+                    "TOPLAM",
+                    ...rows[refRowIndex].map((row) => row.qText),
+               ];
+               refRowIndex += 1;
+          } else {
+               // normal row
+               dim.qSubNodes.forEach((subNode) => {
+                    const type =
+                         subNode.qText === "Totals" ? "subtotal" : "node";
+                    const label =
+                         subNode.qText === "Totals" ? dim.qText : subNode.qText;
+                    rows[refRowIndex] = [
+                         type,
+                         label,
+                         ...rows[refRowIndex].map((row) => row.qText),
+                    ];
+                    refRowIndex += 1;
+               });
+          }
+          //  console.log(dim);
+     });
+     console.log(rows);
+     // rows.forEach((row, rowIndex) => {
+     //      const newRow = ["total", "TOPLAM", ...row];
+     //      console.log(rowIndex);
+     // });
+};
